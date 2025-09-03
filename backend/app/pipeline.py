eb64340f359babab6f42310e3167985f868a1dd1
Joao Victor Rocha <joaov_rocha1@hotmail.com>
2025-09-03 15:52:00 -0300

    changes

--- backend/app/pipeline.py
+++ backend/app/pipeline.py
@@ -1,7 +1,7 @@
 from core.io import to_bgr, to_gray
 from core.denoise import median_filter
-from core.contrast import clahe
-from core.threshold import adaptive_gaussian
+from core.contrast import  equalize
+
 
 def run_pipeline(file_bytes: bytes):
     # 1. Load and convert
@@ -12,9 +12,6 @@ def run_pipeline(file_bytes: bytes):
     denoised = median_filter(gray, 5)
 
     # 3. Enhance contrast
-    enhanced = clahe(denoised)
-
-    # 4. Threshold (binarize)
-    binary = adaptive_gaussian(enhanced, 11, 2)
+    enhanced = equalize(denoised)
 
     return enhanced
--- backend/core/contrast.py
+++ backend/core/contrast.py
@@ -5,7 +5,3 @@ def equalize(gray):
     return cv2.equalizeHist(gray)
 
 
-# Applies CLAHE (adaptive histogram equalization) for local contrast enhancement.
-def clahe(gray, clip_limit: float = 2.0, tile_grid_size=(8,8)):
-    c = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
-    return c.apply(gray)
